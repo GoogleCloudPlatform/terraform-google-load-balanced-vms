57a9ee6ac0811409764caf31ed8c9829980db785
Terrence Ryan <tpryan@google.com>
2022-08-04 13:04:28 -0700

    Trying to get this thing working.

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -18,15 +18,14 @@ module "project" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 11.0"
 
-  name              = "ci-load_balanced_vms"
+  name              = "ci-load-balanced-vms"
   random_project_id = "true"
   org_id            = var.org_id
   folder_id         = var.folder_id
   billing_account   = var.billing_account
-
   activate_apis = [
-    "cloudresourcemanager.googleapis.com",
-    "storage-api.googleapis.com",
-    "serviceusage.googleapis.com"
+    "serviceusage.googleapis.com", 
+    "compute.googleapis.com", 
+    "cloudresourcemanager.googleapis.com"
   ]
 }
