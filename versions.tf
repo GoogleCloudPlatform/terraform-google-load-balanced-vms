6e1ec6c4de6a7a867ac3ee27fa7d9b72eff40e79
bharathkkb <bharathkrishnakb@gmail.com>
2022-11-16 03:50:40 +0000

    chore: fix tflint issues

--- versions.tf
+++ versions.tf
@@ -18,13 +18,27 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     google = {
-      source = "hashicorp/google"
-      # TODO: Check why this filter was breaking tests.
-      # version = "~> 3.53, < 5.0"
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.5"
     }
   }
 
   provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-load-balanced-vms/v0.1.1"
+  }
+  provider_meta "google-beta" {
     module_name = "blueprints/terraform/terraform-google-load-balanced-vms/v0.1.1"
   }
 }
